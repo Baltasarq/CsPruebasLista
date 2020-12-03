d46c130af59d1ed702089ad1afd9607872ce57e5
baltasar <baltasarq@gmail.com>
2020-12-03 14:07:45 +0100

    Added programming by contract

--- PruebasListas/Lista.cs
+++ PruebasListas/Lista.cs
@@ -1,4 +1,8 @@
 // ReSharper disable All
+
+using System;
+using System.Diagnostics;
+
 namespace PruebasListas
 {
     public class Lista<T> {
@@ -24,6 +28,20 @@ namespace PruebasListas
 
         public void Add(T x)
         {
+            Debug.WriteLine( "List.Add()" );
+            Debug.Indent();
+            Debug.Assert( x != null, "Lista.Add(): x es null" );
+
+            int numNodosPpio = this.Conteo();
+            Debug.WriteLine( "Nodos antes de añadir: " + numNodosPpio );
+            
+            #if DEBUG
+                if ( x == null )
+                {
+                    throw new ArgumentException( "x es null en Lista.Add()" );
+                }
+            #endif
+            
             var nodo = new Nodo {
                 Dato = x
             };
@@ -38,7 +56,37 @@ namespace PruebasListas
                 this.fin = nodo;
             }
 
+            this.CompruebaRecorridoLista();
+            
+            Debug.WriteLine( "Nodos despues de añadir: " + this.Conteo() );
+            Debug.Assert( this.fin.Dato.Equals( x ) );
+            Debug.Assert( numNodosPpio == ( this.Conteo() - 1 ), "num. nodos no incrementado en 1" );
+            Debug.Unindent();
             return;
+        }
+
+        [Conditional("DEBUG")]
+        void CompruebaRecorridoLista()
+        {
+            Nodo n = this.ppio;
+
+            while (n != null
+                   && n.Sig != null)
+            {
+                n = n.Sig;
+            }
+
+            Debug.Assert( n == this.fin, "CompruebaRecorrido: no ha llegado al final" );
+            
+            n = this.fin;
+
+            while (n != null
+                   && n.Prev != null)
+            {
+                n = n.Prev;
+            }
+            
+            Debug.Assert( n == this.ppio, "CompruebaRecorrido: no ha llegado al principio" );
         }
 
         public void IrPrincipio()
@@ -75,6 +123,20 @@ namespace PruebasListas
         {
             this.IrPrincipio();
             return this.EsFinal();
+        }
+
+        public int Conteo()
+        {
+            int toret = 0;
+            Nodo n = this.ppio;
+            
+            while ( n != null )
+            {
+                n = n.Sig;
+                ++toret;
+            }
+
+            return toret;
         }
 
         Nodo ppio;
--- PruebasListas/Program.cs
+++ PruebasListas/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace PruebasListas
+﻿using System;
+
+namespace PruebasListas
 {
+    using System.Diagnostics;
+
+    class A
+    {
+        public int X
+        {
+            get;
+            set;
+        }
+    }
+
     internal class Program
     {
+        [Conditional("DEBUG")]
+        private static void PreparaTrace()
+        {
+            Trace.Listeners.Add( new ConsoleTraceListener() );
+            Trace.AutoFlush = true;
+        }
         public static void Main(string[] args)
         {
+            PreparaTrace();
+            Trace.WriteLine( "Empieza el programa" );
+
+            var l = new Lista<A>();
+
+            l.Add( new A{ X = 11 } );
+            l.Add( new A{ X = 22 } );
+            l.Add( new A{ X = 33 } );
+            
+            l.IrPrincipio();
+            while (!l.EsFinal())
+            {
+                Console.WriteLine( l.Actual.X );
+                l.IrSiguiente();
+            }
+            
+            Trace.WriteLine( "Fin de programa" );
+            Trace.Close();
         }
     }
 }
